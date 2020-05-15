8f67fe1727e594530fefe267c849b2d848e11049
rwparrish <rino3717@gmail.com>
2020-05-14 18:51:57 -0600

    videos action finished

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,24 +1,42 @@
 class VideosController < ApplicationController
     
-    get '/videos' do 
+    get '/videos' do
+       if !is_logged_in?
+           redirect '/login'
+       end
+       @vids = current_user.videos
+       @vids = @vids.order(:entry_date)
+       erb :'/videos/index'
     end
 
     get '/videos/new' do 
+        if !is_logged_in?
+            redirect '/login'
+        end
     end
 
     post '/videos/new' do
     end
 
     get '/videos/:id' do
+        if !is_logged_in?
+            redirect '/login'
+        end
     end
 
     get '/videos/:id/edit' do
+        if !is_logged_in?
+            redirect '/login'
+        end
     end
 
     patch '/videos/:id/' do
     end
 
-    delete '/videos/:id' do 
+    delete '/videos/:id' do
+        if !is_logged_in?
+            redirect '/login'
+        end
     end
 
 end
