ea53df5a75378fd818f1ccf339c0f5eb0c134540
rwparrish <rino3717@gmail.com>
2020-05-14 18:38:09 -0600

    framework for actions in videos controller

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,16 +1,24 @@
 class VideosController < ApplicationController
     
-get '/videos' do 
-end
+    get '/videos' do 
+    end
 
-post '/videos' do
-end
+    get '/videos/new' do 
+    end
 
-get '/videos/:id' do
-end
+    post '/videos/new' do
+    end
 
-post '/videos/:id' do
-end
+    get '/videos/:id' do
+    end
 
+    get '/videos/:id/edit' do
+    end
+
+    patch '/videos/:id/' do
+    end
+
+    delete '/videos/:id' do 
+    end
 
 end
